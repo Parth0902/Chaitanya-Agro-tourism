b91b4274587e74b6794fde59830d31151f12f54b
Parth Godase <parthgodase123@gmail.com>
2023-01-15 17:12:23 +0530

    done

---
+++ nav.js
@@ -0,0 +1,13 @@
+// toggle button
+
+
+const tbtn=document.getElementsByClassName('toggle-button')[0]
+const nav=document.getElementsByClassName('links')[0]
+
+
+tbtn.addEventListener('click',()=>{
+  
+   
+   nav.classList.toggle('active1');
+  
+});
